e88c9889e8ef1e066fdca9ed5b61953bfba0a649
Arthur Jardim Giareta Conti <vntarco@venturus.org.br>
2017-12-14 15:36:01 +0000

    Added ButterKnife

--- app/src/main/java/com/conti/autosport/MainActivity.java
+++ app/src/main/java/com/conti/autosport/MainActivity.java
@@ -1,7 +1,14 @@
 package com.conti.autosport;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.btnInvoice)
+    public void startInvoice(View v){
+        startActivity(new Intent(this,InvoiceActivity.class));
     }
 }
